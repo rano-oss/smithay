a54c42c018477eb78b8c917897cf36df7e83d90a
dcz <gilapfco.dcz@porcupinefactory.org>
2025-11-18 11:26:36 +0000

    tracking boundness

--- src/wayland/keyboard_filter/mod.rs
+++ src/wayland/keyboard_filter/mod.rs
@@ -4,11 +4,13 @@
 //!
 //! The current implementation supports filtering for input method purposes.
 
+use std::{collections::HashSet, sync::{Arc, Mutex}};
+
 use tracing::{warn, error, debug};
 
 use wayland_client::WEnum;
 use wayland_server::{backend::GlobalId, protocol::{wl_keyboard::WlKeyboard, wl_surface::WlSurface}, Client, DataInit, Dispatch, DisplayHandle, GlobalDispatch, New, Resource};
-use wl_input_method::keyboard_filter::v1::server::{xx_keyboard_filter_manager_v1::{self, XxKeyboardFilterManagerV1}, xx_keyboard_filter_v1::{self, FilterAction, XxKeyboardFilterV1}};
+use wl_input_method::{input_method::v1::server::xx_input_method_v1::XxInputMethodV1, keyboard_filter::v1::server::{xx_keyboard_filter_manager_v1::{self, XxKeyboardFilterManagerV1}, xx_keyboard_filter_v1::{self, FilterAction, XxKeyboardFilterV1}}};
 
 use crate::{input::{keyboard::KeyboardHandle, SeatHandler}, utils::Serial, wayland::input_method_v3::InputMethodUserData};
 
@@ -24,6 +26,18 @@ pub struct KeyboardFilterManagerGlobalData {
     filter: Box<dyn for<'c> Fn(&'c Client) -> bool + Send + Sync>,
 }
 
+/// Data accesible from XxKeyboardFilterManagerV1
+#[derive(Debug, Default)]
+pub struct KeyboardFilterManagerUserData {
+    inner: Arc<Mutex<KeyboardFilterManagerUserDataInner>>,
+}
+
+#[derive(Debug, Default)]
+struct KeyboardFilterManagerUserDataInner {
+    bound_keyboards: HashSet<WlKeyboard>,
+    bound_ims: HashSet<XxInputMethodV1>,
+}
+
 /// State of the protocol
 #[derive(Debug)]
 pub struct KeyboardFilterManagerState {
@@ -35,7 +49,7 @@ impl KeyboardFilterManagerState {
     pub fn new<D, F>(display: &DisplayHandle, filter: F) -> Self
     where
         D: GlobalDispatch<XxKeyboardFilterManagerV1, KeyboardFilterManagerGlobalData>,
-        D: Dispatch<XxKeyboardFilterManagerV1, ()>,
+        D: Dispatch<XxKeyboardFilterManagerV1, KeyboardFilterManagerUserData>,
         //D: Dispatch<XxInputMethodV1, InputMethodUserData<D>>,
         D: SeatHandler,
         D: 'static,
@@ -59,7 +73,7 @@ impl KeyboardFilterManagerState {
 impl<D> GlobalDispatch<XxKeyboardFilterManagerV1, KeyboardFilterManagerGlobalData, D> for KeyboardFilterManagerState
 where
     D: GlobalDispatch<XxKeyboardFilterManagerV1, KeyboardFilterManagerGlobalData>,
-    D: Dispatch<XxKeyboardFilterManagerV1, ()>,
+    D: Dispatch<XxKeyboardFilterManagerV1, KeyboardFilterManagerUserData>,
     D: 'static,
 {
     fn bind(
@@ -70,7 +84,7 @@ where
         _: &KeyboardFilterManagerGlobalData,
         data_init: &mut DataInit<'_, D>,
     ) {
-        data_init.init(resource, ());
+        data_init.init(resource, KeyboardFilterManagerUserData::default());
     }
 
     fn can_view(client: Client, global_data: &KeyboardFilterManagerGlobalData) -> bool {
@@ -78,9 +92,9 @@ where
     }
 }
 
-impl<D> Dispatch<XxKeyboardFilterManagerV1, (), D> for KeyboardFilterManagerState
+impl<D> Dispatch<XxKeyboardFilterManagerV1, KeyboardFilterManagerUserData, D> for KeyboardFilterManagerState
 where
-    D: Dispatch<XxKeyboardFilterManagerV1, ()>,
+    D: Dispatch<XxKeyboardFilterManagerV1, KeyboardFilterManagerUserData>,
     D: Dispatch<XxKeyboardFilterV1, KeyboardFilterUserData<D>>,
     D: SeatHandler,
     D: 'static,
@@ -88,22 +102,47 @@ where
     fn request(
         _state: &mut D,
         _client: &Client,
-        _: &XxKeyboardFilterManagerV1,
+        resource: &XxKeyboardFilterManagerV1,
         request: xx_keyboard_filter_manager_v1::Request,
-        data: &(),
+        data: &KeyboardFilterManagerUserData,
         _dh: &DisplayHandle,
         data_init: &mut DataInit<'_, D>,
     ) {
         match request {
             xx_keyboard_filter_manager_v1::Request::BindToInputMethod { keyboard, input_method, surface, extensions } => {
+                {
+                    let bind = data.inner.lock().unwrap();
+                    if bind.bound_keyboards.contains(&keyboard) {
+                        resource.post_error(
+                            xx_keyboard_filter_manager_v1::Error::AlreadyBound,
+                            format!("WlKeyboard {keyboard:?} already bound"),
+                        );
+                        return
+                    };
+                    if bind.bound_ims.contains(&input_method) {
+                        resource.post_error(
+                            xx_keyboard_filter_manager_v1::Error::AlreadyBound,
+                            format!("Input method {input_method:?} already bound"),
+                        );
+                        return;
+                    };
+                }
                 let filter = data_init.init::<XxKeyboardFilterV1, _>(
                     extensions,
                     KeyboardFilterUserData {
+                        manager_data: data.inner.clone(),
                       //  keyboard_handle: data.keyboard_handle.clone(),
                         keyboard_handle: None,
+                        bound_keyboard: keyboard.clone(),
+                        bound_input_method: input_method.clone(),
                     },
                 );
-
+                {
+                    let mut bind = data.inner.lock().unwrap();
+                    bind.bound_keyboards.insert(keyboard);
+                    bind.bound_ims.insert(input_method);
+                }
+                
 /*                let mut input_method = input_method.data::<InputMethodUserData<D>>().unwrap();
                     instance.bound_keyboard = Some(Filter {
                         im_keyboard: keyboard,
@@ -130,7 +169,7 @@ macro_rules! delegate_keyboard_filter_manager_v1 {
             $crate::wayland::keyboard_filter::KeyboardFilterManagerGlobalData
         ] => $crate::wayland::keyboard_filter::KeyboardFilterManagerState);
         $crate::reexports::wayland_server::delegate_dispatch!($(@< $( $lt $( : $clt $(+ $dlt )* )? ),+ >)? $ty: [
-            $crate::reexports::wayland_protocols_experimental::keyboard_filter::v1::server::xx_keyboard_filter_manager_v1::XxKeyboardFilterManagerV1: ()
+            $crate::reexports::wayland_protocols_experimental::keyboard_filter::v1::server::xx_keyboard_filter_manager_v1::XxKeyboardFilterManagerV1: KeyboardFilterManagerUserData
         ] => $crate::wayland::keyboard_filter::KeyboardFilterManagerState);
         $crate::reexports::wayland_server::delegate_dispatch!($(@< $( $lt $( : $clt $(+ $dlt )* )? ),+ >)? $ty: [
             $crate::reexports::wayland_protocols_experimental::keyboard_filter::v1::server::xx_keyboard_filter_v1::XxKeyboardFilterV1: $crate::wayland::keyboard_filter::KeyboardFilterUserData<Self>
@@ -178,6 +217,9 @@ pub(crate) struct Filter {
 pub struct KeyboardFilterUserData<D: SeatHandler> {
     //pub(crate) keyboard_handle: KeyboardHandle<D>,
     pub(crate) keyboard_handle: Option<KeyboardHandle<D>>,
+    manager_data: Arc<Mutex<KeyboardFilterManagerUserDataInner>>,
+    bound_keyboard: WlKeyboard,
+    bound_input_method: XxInputMethodV1,
 }
 
 fn get_filter<'a>() -> &'a queue::DispatchQueue {
@@ -224,6 +266,9 @@ where
                 for e in filter.events_to_filter.lock().unwrap().drain(..) {
                     filter.apply(e)
                 }
+                let mut mgr = data.manager_data.lock().unwrap();
+                mgr.bound_keyboards.remove(&data.bound_keyboard);
+                mgr.bound_ims.remove(&data.bound_input_method);
             }
             Request::Filter { serial, action } => {
                 /// Wayland enums are not exhaustive, so they require matching on `_`. We filter out unsupported actions early, so with an exhaustive enum we can let Rust find missing patterns in `match`es later.
@@ -311,7 +356,7 @@ mod test {
     where
         T: wayland_server::Dispatch<
             protocol::xx_keyboard_filter_manager_v1::XxKeyboardFilterManagerV1,
-            (),
+            KeyboardFilterManagerUserData,
         >,
     {
     }
